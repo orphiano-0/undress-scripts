5964c4017f526daf3a1bd1d84cc154187f3220c8
Gabriel Orphiano <gab.orphiano@gmail.com>
2023-07-19 23:21:18 +0800

    Fixed some layout issues

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default async function IndexPage() {
   return (
     <div className="divide-y divide-gray-200 dark:divide-gray-u700">
       <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-6xl sm:leading-10 md:text-6xl
         md:leading-14">
           All Articles
         </h1>
--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -54,7 +54,7 @@ export default async function SlugPage({
           </div>
 
           <div>
-            <h1 className="text-3xl font-extrabold  leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-4xl md:leading-15">
+            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-4xl md:leading-15">
               {data.title}
             </h1>
           </div>
