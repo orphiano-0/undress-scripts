fb4fb0eb1ffbe69348c3850a40a600b44e58640a
Gabriel Orphiano <gab.orphiano@gmail.com>
2023-10-25 16:28:31 +0800

    Update sanity.ts
    Added comment

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,5 +1,6 @@
 import { createClient } from "next-sanity"
 
+# the project id is based from my sanity account
 const projectId = 'baue88l1'
 const dataset = 'production'
 const apiVersion = '2023-01-01'
